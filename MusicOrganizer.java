9d2be585ef871b062bdedb2f32fc5652624c5de3
Forcriun <forcriun@gmail.com>
2018-01-16 14:34:07 +0100

    Ahora muestra los archivos numerados

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -13,7 +13,7 @@ public class MusicOrganizer
     private ArrayList<String> files;
     // A player for the music files.
     private MusicPlayer player;
-        
+
     /**
      * Create a MusicOrganizer
      */
@@ -22,7 +22,7 @@ public class MusicOrganizer
         files = new ArrayList<String>();
         player = new MusicPlayer();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -31,7 +31,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -40,7 +40,7 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
@@ -52,7 +52,7 @@ public class MusicOrganizer
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
@@ -82,13 +82,15 @@ public class MusicOrganizer
     {
         player.stop();
     }
-    
+
     /**
      * Muestra por pantalla todos los objetos de la coleccion.
      */
     public void listAllFiles(){
+        int position = 1;
         for(String filename : files){
-            System.out.println(filename);
+            System.out.println(position + ": " + filename);
+            position++;
         }
     }
 }
