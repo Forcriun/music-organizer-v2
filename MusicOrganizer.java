9b43f6f1c24d92102d384ad20ca803741954f11d
Forcriun <forcriun@gmail.com>
2018-01-16 15:49:44 +0100

    Añadido metodo que muestra los archivos coincidentes

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -93,4 +93,17 @@ public class MusicOrganizer
             position++;
         }
     }
+
+    /**
+     * Enumera los nombres de archivo que se correponden con la cadena de
+     * busqueda proporcionada.
+     * @param searchString La cadena que hay que buscar.
+     */
+    public void listMatching(String searchString){
+        for(String filename : files){
+            if(filename.contains(searchString)){
+                System.out.println(filename);
+            }
+        }
+    }
 }
